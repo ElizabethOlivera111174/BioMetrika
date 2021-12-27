dd3ad0e8af6d6b3a7f1a0d109d848b67e97e8ed9
Luu Olivera <eliolivera570@gmail.com>
2021-12-27 14:15:04 -0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+                if (i % 6==0)
+                {
+                    Console.WriteLine("bio");
+                }
+                else if(i % 9 == 0)
+                {
+                    Console.WriteLine("metrika");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+               
+            }
+            
+        }
+    }
+}
